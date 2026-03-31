4c0f3b2a670c57d4557f62d38656060a2eaf7e2d
Md Kaoser Mahmud <kaosermahmud.cse@gmail.com>
2026-03-31 23:02:08 +0600

    fix ui

--- src/screens/HomeOneScreen.js
+++ src/screens/HomeOneScreen.js
@@ -3139,8 +3139,8 @@ const FoodCard = ({ title, location, isSponsored, img, onPress }) => (
     <View style={styles.cardInfo}>
       <View>
         <Text style={styles.cardTitle}>
-           {title.length > 35 
-    ? `${title.substring(0, 35)}...` 
+           {title.length > 30 
+    ? `${title.substring(0, 30)}...` 
     : title}
         </Text>
         <Text
